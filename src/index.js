8197f3b81f1e52801e0a300c9250ce108fe829e8
zaidrehan <zaidrehan03@gmaill.com>
2024-05-15 06:09:23 -0700

    aaaa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Welcome from './components/Welcome';
+// import Welcome from './components/Welcome';
 // import BhaiApp from './components';
 // import reportWebVitals from './reportWebVitals';
 // import './index.css';
